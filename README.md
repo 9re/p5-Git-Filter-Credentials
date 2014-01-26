e35dbf855eb9895297466316126176692e770ea7
Taro Kobayashi <9re.3000@gmail.com>
2014-01-26 13:51:06 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Git::Filter::Credentials - It's new $module
+
+# SYNOPSIS
+
+    use Git::Filter::Credentials;
+
+# DESCRIPTION
+
+Git::Filter::Credentials is ...
+
+# LICENSE
+
+Copyright (C) Taro Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Taro Kobayashi <9re.3000@gmail.com>
---
+++ lib/Git/Filter/Credentials.pm
@@ -0,0 +1,39 @@
+package Git::Filter::Credentials;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Git::Filter::Credentials - It's new $module
+
+=head1 SYNOPSIS
+
+    use Git::Filter::Credentials;
+
+=head1 DESCRIPTION
+
+Git::Filter::Credentials is ...
+
+=head1 LICENSE
+
+Copyright (C) Taro Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Taro Kobayashi E<lt>9re.3000@gmail.comE<gt>
+
+=cut
+
